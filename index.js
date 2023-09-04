aa6b14194729f327f60f300d341c0a8de78bb057
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-04 21:40:43 +0530

    inser-data

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const express = require("express");
-const reqFilter = require('./middleware')
-const app = express();
-const route = express.Router();
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
+const database = 'e-comm';
+const client = new MongoClient(url);
 
-route.use(reqFilter);
+async function getData() {
+  let result = await client.connect();
+  let db = result.db(database);
+  let collection = db.collection('products');
+  let response = await collection.find({}).toArray();
+  console.log(response);
+}
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Home Page");
-});
-route.get("/profile", (req, res) => {
-  res.send("Welcome to the Profile Page");
-});
-route.get("/about", (req, res) => {
-    res.send("Welcome to the About us Page");
-  });
-
-app.use('/',route);
-app.listen(3000);
-
+getData();
