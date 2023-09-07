5916b56a88bedb77e10f7b5e99cf7c21587863cb
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-09-07 09:03:20 +0530

    CURD with Mongoos

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
+const mongoose = require("mongoose");
 
-const dbConnect = require('./mongodb');
+mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
 
-const main = async () => {
-  let data = await dbConnect();
-  data = await data.find().toArray();
-  console.warn(data);
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
+});
+
+const saveInDB = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  let data = new Product({
+    name: "m8",
+    price: "5000",
+    brand: "max",
+    category: "Mobile",
+  });
+  let result = await data.save();
+
+  console.log(result);
 };
-main();
+
+const updateInDB = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  let data = await Product.updateOne(
+    {name: "m 40"},
+    {
+      $set:{price:370,name:'max 0'},
+    }
+  );
+  console.log(data);
+};
+
+const deleteInDB = async()=>{
+    const product = mongoose.model("products",ProductSchema);
+    let data = await product.deleteOne({name:'max 0'})
+    console.log(data)
+}
+
+const findInDB = async ()=>{
+    const product = mongoose.model("products",ProductSchema);
+    let data = await product.find({name:'u10'});
+    console.log(data)
+}
+findInDB();
